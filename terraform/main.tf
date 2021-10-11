8d0a45a371b5184149be77e2f6801f1d293ee1e0
ume0202 <umesh.nanjundappa@gmail.com>
2021-10-11 17:04:57 +0530

    Modified RG, storage account and container names

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
-    container_name       = "tfstatefile"
+    resource_group_name  = "GithubTerraformAzureDemo"
+    storage_account_name = "gaterraformdemo"
+    container_name       = "tfsfile"
     key                  = "dev.terraform.tfstate"
   }
 }
